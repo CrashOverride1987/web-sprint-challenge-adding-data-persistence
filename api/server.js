5dcd9d866a0fa72d59dedf590521b2399c9900d7
Daniel Brannon <daniel-brannon@hotmail.com>
2021-05-14 10:06:57 -0500

    caught typo

--- api/server.js
+++ api/server.js
@@ -1,7 +1,7 @@
 const express = require('express')
-const projectRouter = require('./projects/router')
-const resourceRouter = require('./resources/router')
-const taskRouter = require('./tasks/router')
+const projectRouter = require('./project/router')
+const resourceRouter = require('./resource/router')
+const taskRouter = require('./task/router')
 
 const server = express()
 
