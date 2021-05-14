eff3fb7544605c0dcef4126c5998600c38cd4867
Daniel Brannon <daniel-brannon@hotmail.com>
2021-05-14 07:27:39 -0500

    wip

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-// start your server here
+require('dotenv').config()
+
+const server = require('./api/server')
+
+const PORT = process.env.PORT
+
+server.listen(PORT, () => console.log(`/nAPI running on port ${PORT}/n`))
